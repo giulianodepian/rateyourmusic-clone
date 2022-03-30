8ccb8b75d664cd92e80526182d88c2534bcb776a
De Pian, Giuliano <yulomax@gmail.com>
2022-03-30 16:14:37 -0300

    Configured Layout, missing Search bar/for and sign up link

--- rateyourmusic-app/src/pages/Layout.js
+++ rateyourmusic-app/src/pages/Layout.js
@@ -2,6 +2,7 @@ import { Outlet, Link } from "react-router-dom";
 import rymlogo from "./../static/images/sonemic-512.png";
 import React from 'react';
 import './../static/styles/Layout.css';
+import Button from 'react-bootstrap/Button';
 
 class Layout extends React.Component {
 	render() {
@@ -13,6 +14,23 @@ class Layout extends React.Component {
 			        <img class="rym-logo-img" src={rymlogo} alt="Logo" />
 			      </Link>
 			    </div>
+			    <div class="rym-menu-separator1" />
+			    <div class="menu-links">
+			      <Link to="/new-music">
+			        new music
+			      </Link>
+			    </div>
+			    <div class="menu-links">
+			      <Link to="/genres">
+			        genres
+			      </Link>
+			    </div>
+			    <div class="menu-links">
+			      <Link to="/charts">
+			        charts
+			      </Link>
+			    </div>
+			    <div class="rym-menu-separator1" />
 			  </div>
 
 			  <Outlet />
